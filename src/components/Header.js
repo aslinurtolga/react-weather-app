b9f1348af30cfcdf329386ad1a28c2e9c9341ddb
AslinurTolga <aslinurtolga@gmail.com>
2022-11-04 14:53:42 +0300

    try and catch added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,12 @@
 import React from "react";
+import { BsCloudMoonFill } from "react-icons/bs";
 
 const Header = () => {
   return (
     <section className="top-banner">
       <div className="container">
         <center>
-          <img
-            src="https://media.giphy.com/media/9G1tb6uVcZ0vJeZSIe/giphy.gif"
-            alt="Bank logo"
-            className="nav__logo"
-            id="logo"
-          />
+          <BsCloudMoonFill size="34px" />
           <h1 className="heading">Weather App</h1>
         </center>
       </div>
--- src/components/Main.js
+++ src/components/Main.js
@@ -5,7 +5,10 @@ import { useEffect } from "react";
 
 const Main = () => {
   const [weatherData, setWeatherData] = useState([]);
-  const [searchText, setSearchText] = useState("ankara");
+  const [searchText, setSearchText] = useState("Istanbul");
+  const [error, setError] = useState("Enter a valid text");
+  const [cityname, setCityName] = useState([]);
+
   let apiKey = process.env.REACT_APP_API_KEY;
   let units = "metric";
   let lang = "tr";
@@ -13,17 +16,40 @@ const Main = () => {
 
   useEffect(() => {
     getCity();
+    setSearchText("");
   }, []);
 
   const getCity = async () => {
-    const { data } = await axios.get(url);
-    setWeatherData([...weatherData, data]);
-    console.log(data);
+    try {
+      if (searchText) {
+        const { data } = await axios.get(url);
+        setWeatherData([...weatherData, data]);
+        console.log(data);
+      }
+    } catch (error) {
+      setError("Invalid enter a text");
+    }
   };
 
   const handlerSubmit = (e) => {
     e.preventDefault();
-    getCity();
+    if (!searchText) {
+      setError("Please enter a search text");
+    } else {
+      if (cityname.includes(searchText)) {
+        setError(`You already entered a ${searchText}`);
+      } else {
+        getCity();
+        setCityName([...cityname, searchText]);
+      }
+      setSearchText("");
+      setTimeout(() => {
+        setError("");
+      }, 1000);
+    }
+    setTimeout(() => {
+      setError("");
+    }, 1000);
   };
 
   return (
@@ -31,12 +57,13 @@ const Main = () => {
       <form onSubmit={handlerSubmit}>
         <input
           onChange={(e) => setSearchText(e.target.value)}
+          value={searchText}
           type="text"
           placeholder="Search for a city"
           autoFocus
         />
         <button type="submit">SUBMIT</button>
-        {/* <span className="msg">error</span> */}
+        <span className="msg">{error}</span>
       </form>
       <div className="container">
         <ul className="cities">
